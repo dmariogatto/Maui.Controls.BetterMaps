6af372697f9f1df92802fcfa3732b3572bd7c052
Daniel Gatto <d.mario.gatto@gmail.com>
2023-05-30 07:07:17 +0930

    Update MKUserTrackingButtonExtensions.cs

--- src/Maui.Controls.BetterMaps/Platforms/iOS/MKUserTrackingButtonExtensions.cs
+++ src/Maui.Controls.BetterMaps/Platforms/iOS/MKUserTrackingButtonExtensions.cs
@@ -7,7 +7,8 @@ namespace Maui.Controls.BetterMaps.iOS
     {
         internal static void UpdateTheme(this MKUserTrackingButton trackingButton, bool isDarkMode)
         {
-            if (trackingButton is null) return;
+            if (trackingButton is null)
+                return;
 
             trackingButton.Layer.BackgroundColor = (isDarkMode ? UIColor.FromRGBA(49, 49, 51, 230) : UIColor.FromRGBA(255, 255, 255, 230)).CGColor;
             trackingButton.Layer.BorderColor = (isDarkMode ? UIColor.FromRGBA(0, 0, 0, 230) : UIColor.FromRGBA(191, 191, 191, 230)).CGColor;
