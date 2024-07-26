e34aaeeeae24b6d5e9be856cc6cd356e05387c10
Daniel Gatto <d.mario.gatto@gmail.com>
2024-07-27 07:41:43 +0930

    vnext

--- src/BetterMaps.Maui/Shared/MapSpan.cs
+++ src/BetterMaps.Maui/Shared/MapSpan.cs
@@ -47,7 +47,7 @@ namespace BetterMaps.Maui
 
         public override bool Equals(object obj)
         {
-            if (ReferenceEquals(null, obj))
+            if (obj is null)
                 return false;
             if (ReferenceEquals(this, obj))
                 return true;
--- src/BetterMaps.Maui/Shared/Pin.cs
+++ src/BetterMaps.Maui/Shared/Pin.cs
@@ -98,7 +98,7 @@ namespace BetterMaps.Maui
 
         public override bool Equals(object obj)
         {
-            if (ReferenceEquals(null, obj))
+            if (obj is null)
                 return false;
             if (ReferenceEquals(this, obj))
                 return true;
